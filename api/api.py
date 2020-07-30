335c9e7b7b6949b729f18db022bb40feab711574
Beau Martin <beaumartin@beaus-mbp.attlocal.net>
2020-07-29 22:52:49 -0400

    Added mongo functionality to backend

--- api/api.py
+++ api/api.py
@@ -1,8 +1,39 @@
 import time
-from flask import Flask
+from flask import Flask, request, Response
+from pymongo import MongoClient
+from bson.json_util import dumps
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client['test']
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/get-<item>', methods=['GET'])
+def getThings(item=None):
+	collection = db[item]
+	r = Response(response = dumps(collection.find()), mimetype="application/json")
+	return r
+
+@app.route('/add-<item>', methods=['POST'])
+def addThing(item=None):
+	collection = db[item]
+	#Returns the id of the added item
+	postId = collection.insert_one(request.json).inserted_id
+	r = Response(response = dumps(postId), mimetype="application/json")
+	return r
+
+@app.route('/remove-<item>', methods=['DELETE'])
+def removeThing(item=None):
+	collection = db[item]
+	if (request.args.get('_id') != None):
+		args = {'_id': ObjectId(request.args.get('_id'))}
+	else:
+		args = request.args
+	item = collection.find_one_and_delete(args)
+	r = Response(response = dumps(item), mimetype="application/json")
+	return r
