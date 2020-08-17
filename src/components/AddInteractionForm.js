00787de66dbdf1bd6d600333d0e3e9e217a48410
Beau Martin <beaumartin@beaus-mbp.attlocal.net>
2020-08-17 19:55:15 -0400

    All add components functioning

--- src/components/AddInteractionForm.js
+++ src/components/AddInteractionForm.js
@@ -10,51 +10,115 @@ class AddInteractionForm extends Component {
             date: '',
             time: '',
             location: '',
-            people: []
+            interviewee: '',
+            usedHypos: [],
+            people: [],
+            hypos: []
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleMultiChange = this.handleMultiChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
-        const fetched = fetch('/get-people', {
+        
+        //Get people
+        const fetchedPeople = fetch('/get-people', {
             method: 'GET'
         });
 
-        //console.log(fetched)
-
-        fetched.then(response => 
+        fetchedPeople.then(response => 
             response.json().then(data => {
-                //console.log(data)
                 this.setState({
                     people: {data}
                 }); 
             }));
+
+        //Get notnots
+        const fetchedHypos = fetch('/get-notnot')
+
+        fetchedHypos.then(response => 
+            response.json().then(data => {
+                this.setState({
+                    hypos: {data}
+                });
+            }));
+
     }
 
     handleChange(event) {
         const target = event.target;
         const value = target.value;
         const name = target.name;
+
+        //console.log(target)
+        //console.log(value)
     
         this.setState({
           [name]: value
         });
       }
 
+      handleMultiChange (e) {
+        var options = e.target.options;
+        var usedHypos = this.state.usedHypos;
+        for (var i = 0, l = options.length; i < l; i++) {
+          if (options[i].selected) {
+            //console.log(usedHypos.includes(options[i]))
+            if (!usedHypos.includes(options[i].value)) {
+                usedHypos.push(options[i].value);
+            } else {
+                usedHypos = usedHypos.filter(hypo => hypo != options[i].value);
+            }
+        }
+        this.setState({usedHypos: usedHypos});
+      }
+    }
+
+    handleSubmit(event) {
+        const toSubmit = {
+            date: this.state.date,
+            time: this.state.time,
+            location: this.state.location,
+            interviewee: this.state.interviewee,
+            notnots: this.state.usedHypos
+        }
+
+        const fetched = fetch('/add-dpi', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(toSubmit)
+        }); 
+        //Reset all to no true 
+        Object.keys(this.state).map(
+            i => this.setState({ [i]: ''})
+          )
+    }
+
     render() {
         const { people } = this.state;
 
+        const { hypos } = this.state;
+
         let peopleList = people.hasOwnProperty('data') && people.data.map((item, i) => {
-            //console.log(item._id);
+            //console.log(typeof item._id.$oid);
             return (
-                <option key={i} value={item._id}>{item.firstName} {item.lastName}</option>
+                <option key={i} value={item._id.$oid}>{item.firstName} {item.lastName}</option>
               )
             }, this);
 
+        let hypolist = hypos.hasOwnProperty('data') && hypos.data.map((item, i) => {
+            return (
+                <option key={i} value={item._id.$oid}>{item.text}</option>
+            )
+        }, this)
+
 
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label>
                     <h4>Date:</h4>
                     <input
@@ -84,10 +148,25 @@ class AddInteractionForm extends Component {
                 <br />
                 <label>
                     <h4>Interviewee:</h4>
-                    <select>
+                    <select
+                        name="interviewee"
+                        value={this.state.interviewee}
+                        onChange={this.handleChange}>
                         {peopleList}
                     </select>
                 </label>
+                <br />
+                <label>
+                    <h4>Hypotheses:</h4>
+                    <select multiple
+                        name="usedHypos"
+                        value={this.state.usedHypos}
+                        onChange={this.handleMultiChange}>
+                        {hypolist}
+                    </select>
+                </label>
+                <br />
+                <input type="submit" value="Submit" />
             </form>
         )
     }
